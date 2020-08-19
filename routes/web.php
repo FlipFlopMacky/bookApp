8b72885966d7c59932c4ddeb4272617a839ff9c7
FlipFlopMacky <>
2020-08-20 00:47:47 +0900

    本を追加する処理の実装

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,21 @@ Route::get('/', function () {
     $books = Book::all();
     return view('books', ['books' => $books]);
 });
+
+Route::post('/book', function(Request $request) {
+  $validator = Validator::make($request->all(),[
+    'name' => 'required|max:255',
+  ]);
+
+  if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+  }
+
+  $book = new Book;
+  $book->title = $request->name;
+  $book->save();
+
+  return redirect('/');
+});
